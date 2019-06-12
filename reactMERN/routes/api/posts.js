ab96678a51dac094e68dd2187d50917cfddf024d
Louis kane <lkane02@yahoo.com>
2019-06-11 17:54:55 -0700

    create initial post route

--- reactMERN/routes/api/posts.js
+++ reactMERN/routes/api/posts.js
@@ -3,13 +3,47 @@ const router = express.Router();
 const { check, validationResult } = require("express-validator/check");
 const auth = require("../../middleware/auth");
 
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
 // @route  POST api/POSTS
 // @desc   create a post
 // @access Private
 router.post(
   "/",
-  [auth, [check("text", "Text is required").not()]],
-  (req, res) => {}
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationsResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      const post = await newPost.save();
+
+      res.json(post); // once post is saved we will get it back in the response
+    } catch (error) {
+      console.error(err.message);
+      res.status(500).send("server error");
+    }
+  }
 );
 
 module.exports = router;
